24483fe03a38252e228516c6f3f92bf80f040290
TylerGwynn <tylergwynn23@gmail.com>
2022-02-23 20:58:19 -0500

    added some more basic classes I want

---
+++ Assets/oasty/BeatMover.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class BeatMover : MonoBehaviour
+{
+    // An abstract class that reacts to input from the GlobalTimer
+
+    public void OnBeat(){
+
+    }
+
+    public void OnBar(){
+        
+    }
+
+}
--- Assets/oasty/GlobalTimer.cs
+++ Assets/oasty/GlobalTimer.cs
@@ -4,15 +4,49 @@ using UnityEngine;
 
 public class GlobalTimer : MonoBehaviour
 {
+    public int BPM = 120;
+    public int TIME_SIGNATURE = 4; //beats per measure
+    private int beatTimerDefault;
+    public int beat; //value from 0 to 3 indicating how far along the "bar" we are
+    public int bar; // increasing value indication how many measures from the start we are
+    public float beatTimer; //decreasing value to the next "beat"
+    public List<BeatMover> listeners;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        beatTimer = beatTimerDefault;
+        beatTimerDefault = 60/BPM;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        BeatTick();
+    }
+
+    // increments timers/beats based on Time.deltaTime
+    void BeatTick(){
+        beatTimer -= Time.deltaTime;
+        if (beatTimer < 0){
+            beat++;
+            beatTimer = beatTimerDefault;
+            if (beat <= TIME_SIGNATURE){
+                bar++;
+                beat = 0;
+                
+            }
+        }
+    }
+
+    void SendBeat(){
+        foreach (BeatMover x in listeners){
+            x.OnBeat();
+        }
+    }
+    void SendBar(){
+        foreach (BeatMover x in listeners){
+            x.OnBar();
+        }
     }
 }
