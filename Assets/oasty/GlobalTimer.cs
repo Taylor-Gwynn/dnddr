4f3c5613f2916c4b6b49dfc2cc666e42d59a326a
TylerGwynn <tylergwynn23@gmail.com>
2022-02-25 16:31:33 -0500

    added audio metronome for testing

--- Assets/oasty/GlobalTimer.cs
+++ Assets/oasty/GlobalTimer.cs
@@ -12,10 +12,12 @@ public class GlobalTimer : MonoBehaviour
     private int beat;                   // Value from 1 to TIME_SIGNATURE indicating how far along the bar we are
     private int bar;                    // The bar we're current in (starting at 1)
     private float beatTimer;            // Time until the next beat
+    private AudioSource au;
 
     // Start is called before the first frame update
     void Start()
     {
+        au = GetComponent<AudioSource>();
         beatTimer = beatTimerDefault;
         beat = 1;
         bar = 1;
@@ -51,12 +53,20 @@ public class GlobalTimer : MonoBehaviour
     }
 
     void SendBeat(){
+        if (au != null){
+            au.pitch = 1;
+            au.Play();
+        }
         // Debug.Log("Beat: " + beat);
         foreach (BeatMover x in listeners){
             x.OnBeat();
         }
     }
     void SendBar(){
+        if (au != null){
+            au.pitch = 2;
+            au.Play();
+        }
         // Debug.Log("Bar:  " + bar);
         foreach (BeatMover x in listeners){
             x.OnBar();
