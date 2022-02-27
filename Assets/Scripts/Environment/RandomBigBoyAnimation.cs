ed1ce7fb138b7397e258feba7f745d78693557a3
BimChafee <bryan.chafee@gmail.com>
2022-02-27 05:01:33 -0500

    rid the project of unity editor code

--- Assets/Scripts/Environment/RandomBigBoyAnimation.cs
+++ Assets/Scripts/Environment/RandomBigBoyAnimation.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Collections.Generic;
-using UnityEditor.Animations;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
@@ -8,23 +7,23 @@ namespace Environment
 {
     public class RandomBigBoyAnimation : MonoBehaviour
     {
-        public Animator _Animator;
-        public List<AnimatorController> _List;
-
-        private void OnEnable()
-        {
-            SetRandomController();
-        }
-
-        private void OnDisable()
-        {
-            SetRandomController();
-        }
-
-        public void SetRandomController()
-        {
-            int rand = Random.Range(0, _List.Count);
-            _Animator.runtimeAnimatorController = _List[rand];
-        }
+        // public Animator _Animator;
+        // public List<AnimatorController> _List;
+        //
+        // private void OnEnable()
+        // {
+        //     SetRandomController();
+        // }
+        //
+        // private void OnDisable()
+        // {
+        //     SetRandomController();
+        // }
+        //
+        // public void SetRandomController()
+        // {
+        //     int rand = Random.Range(0, _List.Count);
+        //     _Animator.runtimeAnimatorController = _List[rand];
+        // }
     }
 }
